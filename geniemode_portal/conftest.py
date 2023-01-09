4b74d0395cf9c2e3e93bed4873dfd486314df511
r-fine <saiadul.arfain@tuta.io>
2023-01-09 08:50:05 +0600

    first

---
+++ geniemode_portal/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from geniemode_portal.users.models import User
+from geniemode_portal.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
