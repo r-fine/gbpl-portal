6c7c1169bd401c3af4f6ed166c5ae0147fda989c
r-fine <saiadul.arfain@tuta.io>
2023-01-12 00:52:28 +0600

    update 1 on attendance model

--- geniemode_portal/apps/attendance/admin.py
+++ geniemode_portal/apps/attendance/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Attendance
+
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    '''Admin View for Attendance'''
+
+    list_display = ('user', 'date', 'is_present', 'status')
+    list_filter = ('date',)
+    readonly_fields = ('created_at',)
+    search_fields = ('user',)
+    list_editable = ('is_present', 'status')
+    list_per_page = 25
--- geniemode_portal/apps/attendance/models.py
+++ geniemode_portal/apps/attendance/models.py
@@ -4,15 +4,21 @@ from django.utils.translation import gettext_lazy as _
 from geniemode_portal.apps.users.models import User
 
 
+DAYS = (
+    ('Mon', 'Monday'), ('Tue', 'Tuesday'), ('Wed', 'Wednesday'), ('Thu',
+                                                                  'Thursday'), ('Fri', 'Friday'), ('Sat', 'Saturday'), ('Sun', 'Sunday'),
+)
+
+
 class Attendance(models.Model):
 
     user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
 
-    day = models.CharField(_("Day"), max_length=50)
+    day = models.CharField(_("Day"), choices=DAYS, max_length=10)
 
     date = models.DateField(_("Date"), auto_now=False, auto_now_add=False)
 
-    regular_hours = models.CharField(_("Regular Hours"), max_length=50)
+    regular_hours = models.CharField(_("Regular Hours"), max_length=50, blank=True)
 
     in_time = models.TimeField(_("In Time"), auto_now=False, auto_now_add=False)
 
@@ -20,21 +26,21 @@ class Attendance(models.Model):
 
     work_from_home = models.BooleanField(_("Work From Home"), default=False)
 
-    out_office_from = models.TimeField(_("Out Office From"), auto_now=False, auto_now_add=False)
+    out_office_from = models.TimeField(_("Out Office From"), auto_now=False, auto_now_add=False, blank=True, null=True)
 
-    out_office_to = models.TimeField(_("Out Office To"), auto_now=False, auto_now_add=False)
+    out_office_to = models.TimeField(_("Out Office To"), auto_now=False, auto_now_add=False, blank=True, null=True)
 
-    out_reason = models.TimeField(_("Out Reason"), auto_now=False, auto_now_add=False)
+    out_reason = models.CharField(_("Out Reason"), max_length=255, blank=True)
 
-    status = models.CharField(_("Status"), max_length=50)
+    status = models.CharField(_("Status"), max_length=50, blank=True)
 
-    remarks = models.CharField(_("Remarks"), max_length=50)
+    remarks = models.CharField(_("Remarks"), max_length=255, blank=True)
 
-    created_at = models.DateTimeField(_("Entry Created"), auto_now_add=True, editable=False)
+    created_at = models.DateTimeField(_("Entry Created At"), auto_now_add=True, editable=False)
 
     updated_at = models.DateTimeField(_("Entry Updated"), auto_now=True)
 
-    is_done = models.BooleanField(_("Done"), default=False)
+    is_present = models.BooleanField(_("Is Present"), default=False)
 
     class Meta:
 
@@ -47,4 +53,3 @@ class Attendance(models.Model):
     def get_absolute_url(self):
 
         return ('')
-
