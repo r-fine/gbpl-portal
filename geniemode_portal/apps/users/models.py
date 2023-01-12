4a6cdfc3bd194be9f75f707716b556588d4d1cd2
r-fine <saiadul.arfain@tuta.io>
2023-01-12 07:11:24 +0600

    added additional fields to User model

--- geniemode_portal/apps/users/models.py
+++ geniemode_portal/apps/users/models.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField, EmailField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+
+
+DEPARTMENTS = (('A', 'A'), ('B', 'B'), ('C', 'C'),)
+DESIGNATION = (('X', 'X'), ('Y', 'Y'), ('Z', 'Z'),)
 
 
 class User(AbstractUser):
@@ -12,10 +16,19 @@ class User(AbstractUser):
     """
 
     #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    email = EmailField(verbose_name='E-mail Address', unique=True)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    email = models.EmailField(_("E-mail Address"), unique=True)
+    phone = models.CharField(_("Phone Number"), max_length=11, null=True)
+    designation = models.CharField(_("Department"), choices=DESIGNATION, max_length=50, null=True)
+    department = models.CharField(_("Department"), choices=DEPARTMENTS, max_length=50, null=True)
+    profile_pic = models.ImageField(
+        verbose_name='Profile Picture',
+        upload_to="images/",
+        default="images/500_500.png"
+    )
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
@@ -27,8 +40,3 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
-    
-
-    @property
-    def full_name(self):
-        return f'{self.first_name} {self.last_name}'
