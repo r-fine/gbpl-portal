88a2da41a8fba621099d88a9f77e2aba81996653
r-fine <saiadul.arfain@tuta.io>
2023-01-25 12:19:59 +0600

    Create EmailAddress object on user creation

--- geniemode_attendance/apps/users/signals.py
+++ geniemode_attendance/apps/users/signals.py
@@ -5,6 +5,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 from django.template.loader import render_to_string
 
+from allauth.account.models import EmailAddress
 from .models import User
 
 current_site = Site.objects.get_current()
@@ -13,24 +14,30 @@ site_name = current_site.name
 
 
 @receiver(post_save, sender=User)
-def send_email_on_user_creation(sender, instance, **kwargs):
-    email = instance.email
-    s = email.split('@')[0]
-    password = s[0] + s[-1] + '@gbpl23'
-    change_password_url = site_domain + '/accounts/password/change/'
+def send_email_on_user_creation(sender, instance, created, **kwargs):
+    if created:
+        email_address = EmailAddress.objects.create(
+            user_id=instance.id, email=instance.email, verified=True, primary=True
+        )
+        email_address.save()
 
-    context = {
-        'site_domain': site_domain,
-        'site_name': site_name,
-        'email': email,
-        'password': password,
-        'change_password_url': change_password_url,
-    }
+        email = instance.email
+        s = email.split('@')[0]
+        password = s[0] + s[-1] + '@gbpl23'
+        change_password_url = site_domain + '/accounts/password/change/'
 
-    html_path = str(settings.APPS_DIR) + '/templates/account/new_user_creation_email.html'
-    msg_html = render_to_string(html_path, context)
+        context = {
+            'site_domain': site_domain,
+            'site_name': site_name,
+            'email': email,
+            'password': password,
+            'change_password_url': change_password_url,
+        }
 
-    subject = 'New User Created on ' + site_domain
-    recipient = [instance.email]
+        html_path = str(settings.APPS_DIR) + '/templates/account/new_user_creation_email.html'
+        msg_html = render_to_string(html_path, context)
 
-    send_mail(subject, '', settings.DEFAULT_FROM_EMAIL, recipient, html_message=msg_html)
+        subject = 'New User Created on ' + site_domain
+        recipient = [instance.email]
+
+        send_mail(subject, '', settings.DEFAULT_FROM_EMAIL, recipient, html_message=msg_html)
